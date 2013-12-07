146157c85a88fffffe6cfef240cfe27a37a796e6
Nathan Friedly <nathan@nfriedly.com>
2013-12-06 19:46:01 -0500

    adding jshint and jsbeautify, tweaking test paths

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 var cp = require('child_process'),
     assert = require('assert'),
     http = require('http'),
-    request = require('request');
+    request = require('request'),
+    gatlingPath = __dirname + '/../gatling';
 
 describe("Gatling", function() {
     it("should start the test server and send a {type: 'ready'} message", function(done) {
-        var g = cp.fork('../gatling', ['./test/app.js', '--quiet']).once('message', function(msg) {
+        var g = cp.fork(gatlingPath, ['./test/app.js', '--quiet']).once('message', function(msg) {
             assert('ready', msg.type);
             g.on('close', function() {
                 done();
@@ -15,7 +16,7 @@ describe("Gatling", function() {
     });
     
     it("should allow the app to respond to requests", function(done) {
-        var g = cp.fork('../gatling', ['./test/app.js', '--quiet']).once('message', function(msg) {
+        var g = cp.fork(gatlingPath, ['./test/app.js', '--quiet']).once('message', function(msg) {
             request('http://localhost:8080/ok', function(err, data) {
                 if (err) return done(err);
                 
@@ -30,7 +31,7 @@ describe("Gatling", function() {
     });
     
     it("should not allow errors in one request to kill another request", function(done) {
-        var g = cp.fork('../gatling', ['./test/app.js', '--quiet']).once('message', function(msg) {
+        var g = cp.fork(gatlingPath, ['./test/app.js', '--quiet']).once('message', function(msg) {
             request('http://localhost:8080/slow', function(err, data) {
                 if (err) return done(err);
                 
@@ -49,7 +50,7 @@ describe("Gatling", function() {
     });
     
     it("should bring up new workers after one dies", function(done) {
-        var g = cp.fork('../gatling', ['./test/app.js', '--quiet']).once('message', function(msg) {
+        var g = cp.fork(gatlingPath, ['./test/app.js', '--quiet']).once('message', function(msg) {
             request('http://localhost:8080/error', function(err, data) {
                 request('http://localhost:8080/ok', function(err, data) {
                     if (err) return done(err);
@@ -65,4 +66,4 @@ describe("Gatling", function() {
             
         });
     });
-})
+});
